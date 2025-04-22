6d843c4b6b37db2680d297091d9e7513efcbbd30
donp <dpreuss@starfishstorage.com>
2025-04-22 15:54:14 -0400

    Update configuration files to prevent Postgres auto-upgrade

--- redash/__init__.py
+++ redash/__init__.py
@@ -14,7 +14,7 @@ from redash.app import create_app  # noqa
 from redash.destinations import import_destinations
 from redash.query_runner import import_query_runners
 
-__version__ = "25.2.0.dev202504221544"
+__version__ = "25.2.0.dev202504221548"
 
 
 if os.environ.get("REMOTE_DEBUG"):
